2fbc747c3afa6572a84409a355bbe543706a78a4
Reetik Basniwal <reetikbasniwal@gmail.com>
2025-10-10 09:33:30 +0530

    jwt auth

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import psycopg2
 # Local Imports
 from . import models
 from .database import engine
-from .routers import post, user
+from .routers import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -25,3 +25,4 @@ app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
--- utils.py
+++ utils.py
@@ -3,3 +3,7 @@ from passlib.hash import pbkdf2_sha256
 
 def hash(pwd: str):
     return pbkdf2_sha256.hash(pwd)
+
+
+def verify(plain_password, hashed_password):
+    return pbkdf2_sha256.verify(plain_password, hashed_password)

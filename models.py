888e3303c8491b85f198ce54c85f46c63ce60694
Reetik Basniwal <reetikbasniwal@gmail.com>
2025-10-12 19:58:44 +0530

    add relationships

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
+# SQLAlchemy Imports
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+# Local Imports
 from .database import Base
 
 class Post(Base):
@@ -12,6 +15,8 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"

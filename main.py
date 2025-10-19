6ff2f131bd00231d8ea2045676b14bd80580ef36
Reetik Basniwal <reetikbasniwal@gmail.com>
2025-10-19 15:15:53 +0530

    Commit changes before moving git repo to root

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # Local Imports
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
 
-
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
